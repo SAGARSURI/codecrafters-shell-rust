da7f8682b778ceb92c1dd11d06eb4ae6ca9d74e8
Sagar Suri <sagarsuri56@gmail.com>
2024-07-28 13:36:55 +0530

    7th stage

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
-use std::process::exit;
+use std::{env, process::exit};
 
 fn main() {
     loop {
@@ -20,21 +20,55 @@ fn main() {
             }
             Some(&cmd) if cmd == Commands::Echo.to_string() => println!("{}", args.join(" ")),
             Some(&cmd) if cmd == Commands::Type.to_string() => {
-                if let Some(&first_arg) = args.first() {
-                    match first_arg {
-                        arg if arg == Commands::Echo.to_string()
-                            || arg == Commands::Exit.to_string()
-                            || arg == Commands::Type.to_string() =>
-                        {
-                            println!("{} is a shell builtin", arg);
+                match env::var("PATH") {
+                    Ok(sys_path) => {
+                        if let Some(&first_arg) = args.first() {
+                            match first_arg {
+                                arg if arg == Commands::Echo.to_string()
+                                    || arg == Commands::Exit.to_string()
+                                    || arg == Commands::Type.to_string() =>
+                                {
+                                    println!("{} is a shell builtin", arg);
+                                }
+                                _ => match get_path(sys_path, first_arg) {
+                                    Some(path) => {
+                                        println!("{} is {}", first_arg, path);
+                                    }
+                                    _ => {
+                                        println!("{}: not found", first_arg);
+                                    }
+                                },
+                            }
                         }
-                        _ => println!("{}: not found", first_arg),
                     }
+                    _ => {}
                 }
+                // if let Some(&first_arg) = args.first() {
+                //     match first_arg {
+                //         arg if arg == Commands::Echo.to_string()
+                //             || arg == Commands::Exit.to_string()
+                //             || arg == Commands::Type.to_string() =>
+                //         {
+                //             println!("{} is a shell builtin", arg);
+                //         }
+                //         _ => println!("{}: not found", first_arg),
+                //     }
+                // }
             }
             _ => println!("{}: command not found", input),
         }
     }
+}
+
+fn get_path(sys_path: String, cmd: &str) -> Option<String> {
+    let paths: Vec<&str> = sys_path.split(':').collect();
+    for path in paths {
+        let full_path = format!("{}/{}", path, cmd);
+        if std::fs::metadata(&full_path).is_ok() {
+            return Some(full_path);
+        }
+    }
+    return None;
 }
 
 fn read_line() -> String {
