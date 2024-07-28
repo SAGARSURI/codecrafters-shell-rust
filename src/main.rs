6e3d3e6f65cb7629559d45ec4c972182c5c326b3
Sagar Suri <sagarsuri56@gmail.com>
2024-07-28 08:50:25 +0530

    sixth stage

--- src/main.rs
+++ src/main.rs
@@ -13,12 +13,22 @@ fn main() {
         let command = parts.get(0);
         let args = parts.get(1..).unwrap_or_default();
         match command {
-            Some(&"exit") => {
+            Some(&cmd) if cmd == Commands::Exit.to_string() => {
                 if !args.is_empty() {
                     exit(args[0].parse::<i32>().unwrap_or(0));
                 }
             }
-            Some(&"echo") => println!("{}", args.join(" ")),
+            Some(&cmd) if cmd == Commands::Echo.to_string() => println!("{}", args.join(" ")),
+            Some(&cmd) if cmd == Commands::Type.to_string() => {
+                if let Some(&first_arg) = args.first() {
+                    match first_arg {
+                        arg if arg == Commands::Echo.to_string() || arg == Commands::Exit.to_string() => {
+                            println!("{} is a shell builtin", arg);
+                        }
+                        _ => println!("{}: command not found", first_arg)
+                    }
+                }
+            }
             _ => println!("{}: command not found", input),
         }
     }
@@ -29,4 +39,20 @@ fn read_line() -> String {
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
     input.trim().to_string()
+}
+
+enum Commands {
+    Echo,
+    Exit,
+    Type,
+}
+
+impl Commands {
+    fn to_string(&self) -> &str {
+        match self {
+            Commands::Echo => "echo",
+            Commands::Exit => "exit",
+            Commands::Type => "type",
+        }
+    }
 }
