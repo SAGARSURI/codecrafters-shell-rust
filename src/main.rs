23ede25eea44c1d965470f85bb85bf40ac403197
Sagar Suri <sagarsuri56@gmail.com>
2024-08-26 18:00:39 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -20,53 +20,53 @@ fn main() {
             }
             Some(&cmd) if cmd == Commands::Echo.to_string() => println!("{}", args.join(" ")),
             Some(&cmd) if cmd == Commands::Type.to_string() => {
-                match env::var("PATH") {
-                    Ok(sys_path) => {
-                        if let Some(&first_arg) = args.first() {
-                            match first_arg {
-                                arg if arg == Commands::Echo.to_string()
-                                    || arg == Commands::Exit.to_string()
-                                    || arg == Commands::Type.to_string() =>
-                                {
-                                    println!("{} is a shell builtin", arg);
-                                }
-                                _ => match get_path(sys_path, first_arg) {
-                                    Some(path) => {
-                                        println!("{} is {}", first_arg, path);
-                                    }
-                                    _ => {
-                                        println!("{}: not found", first_arg);
-                                    }
-                                },
-                            }
+                if let Some(&first_arg) = args.first() {
+                    match first_arg {
+                        arg if arg == Commands::Echo.to_string()
+                            || arg == Commands::Exit.to_string()
+                            || arg == Commands::Type.to_string() =>
+                        {
+                            println!("{} is a shell builtin", arg);
                         }
+                        _ => match get_path(first_arg) {
+                            Some(path) => {
+                                println!("{} is {}", first_arg, path);
+                            }
+                            _ => println!("{}: not found", first_arg),
+                        },
                     }
-                    _ => {}
                 }
-                // if let Some(&first_arg) = args.first() {
-                //     match first_arg {
-                //         arg if arg == Commands::Echo.to_string()
-                //             || arg == Commands::Exit.to_string()
-                //             || arg == Commands::Type.to_string() =>
-                //         {
-                //             println!("{} is a shell builtin", arg);
-                //         }
-                //         _ => println!("{}: not found", first_arg),
-                //     }
-                // }
+            }
+            Some(&full_path) if get_path(command.unwrap()).is_some() => {
+                let output = execute_program(full_path, args);
+                println!("{}", output.unwrap());
             }
             _ => println!("{}: command not found", input),
         }
     }
 }
 
-fn get_path(sys_path: String, cmd: &str) -> Option<String> {
-    let paths: Vec<&str> = sys_path.split(':').collect();
-    for path in paths {
-        let full_path = format!("{}/{}", path, cmd);
-        if std::fs::metadata(&full_path).is_ok() {
-            return Some(full_path);
+fn get_path(cmd: &str) -> Option<String> {
+    let sys_path_result = env::var("PATH");
+    match sys_path_result {
+        Ok(sys_path) => {
+            let paths: Vec<&str> = sys_path.split(':').collect();
+            for path in paths {
+                let full_path = format!("{}/{}", path, cmd);
+                if std::fs::metadata(&full_path).is_ok() {
+                    return Some(full_path);
+                }
+            }
         }
+        Err(_) => return None,
+    }
+    return None;
+}
+
+fn execute_program(path: &str, args: &[&str]) -> Option<String> {
+    let output = std::process::Command::new(path).args(args).output();
+    if output.is_ok() {
+        return Some(String::from_utf8_lossy(&output.unwrap().stdout).trim().to_string());
     }
     return None;
 }
